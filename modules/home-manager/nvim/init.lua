351ade577ded4ae92418acbb4fcce45b8a9f43da
Joshua Niemela <josh@jniemela.dk>
2022-12-07 00:03:46 +0100

    .

--- modules/home-manager/nvim/init.lua
+++ modules/home-manager/nvim/init.lua
@@ -1,187 +1,87 @@
 local opt = vim.opt
 local g = vim.g
 
-
-vim.cmd [[
-    set nobackup
-    set nowritebackup
-    set noswapfile
-    set noerrorbells
-]]
-
--- Set leader key
-g.mapleader = " "
-
--- Undo files
-opt.undofile = true
-opt.undodir = "/home/josh/.local/share/nvim/undo"
-
--- Indentation
-opt.smartindent = true
-opt.autoindent = true
-opt.tabstop = 2
-opt.shiftwidth = 2
-opt.softtabstop = 2
-opt.expandtab = true
-
-opt.scrolloff = 5
-opt.updatetime = 500
-opt.showmatch = true
--- Clipboard mode
-opt.clipboard = "unnamedplus"
-
--- Allow mouse
-opt.mouse = "a"
-
--- UI
-opt.termguicolors = true -- causing weird colors
-opt.cursorline = true
-
--- Hybrid line numbers
-opt.number = true
-opt.relativenumber = true
-
--- Remove viminfo
-opt.viminfo = ""
-opt.viminfofile = "NONE"
-
--- QoL
-opt.smartcase = true
--- opt.ttimeoutlen = 5
-opt.incsearch = true
-opt.hlsearch = true
-opt.autoread = true
-
-require'nvim-treesitter.configs'.setup = {
-    ensure_installed = "maintained",
-    highlight = {
-        enable = true,
-    },
-}
-
--- Latex
-g.vimtex_view_method = "zathura"
-g.vimtex_quickfix_mode = 0
-g.vimtex_flavor = "latex"
-g.vimtex_conceal = "abdmgs"
-vim.cmd [[ 
-    set conceallevel=2
-    hi clear Conceal
-]]
-
-
-opt.completeopt = {'menu', 'menuone', 'noselect'}
-
-local lspconfig = require('lspconfig')
-local lsp_defaults = lspconfig.util.default_config
-
-lsp_defaults.capabilities = vim.tbl_deep_extend(
-  'force',
-  lsp_defaults.capabilities,
-  require('cmp_nvim_lsp').default_capabilities()
-)
-lspconfig.sumneko_lua.setup({
-    single_file_support = true,
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
-    }
-})
-
-lspconfig.rnix.setup({
-
-})
-
-local cmp = require('cmp')
-local luasnip = require('luasnip')
-
-local select_opts = {behavior = cmp.SelectBehavior.Select}
-
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end
-  },
-  sources = {
-    {name = 'path'},
-    {name = 'nvim_lsp', keyword_length = 2},
-    {name = 'buffer', keyword_length = 2},
-    {name = 'luasnip', keyword_length = 1},
-  },
-  window = {
-    documentation = cmp.config.window.bordered()
-  },
-  formatting = {
-    fields = {'menu', 'abbr', 'kind'},
-    format = function(entry, item)
-      local menu_icon = {
-        nvim_lsp = 'λ',
-        luasnip = '⋗',
-        buffer = 'Ω',
-        path = 'X',
-      }
-
-      item.menu = menu_icon[entry.source.name]
-      return item
-    end,
-  },
-  mapping = {
-    ['<Up>'] = cmp.mapping.select_prev_item(select_opts),
-    ['<Down>'] = cmp.mapping.select_next_item(select_opts),
-
-    ['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
-    ['<C-n>'] = cmp.mapping.select_next_item(select_opts),
-
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
-
-    ['<C-d>'] = cmp.mapping(function(fallback)
-      if luasnip.jumpable(1) then
-        luasnip.jump(1)
-      else
-        fallback()
-      end
-    end, {'i', 's'}),
-
-    ['<C-b>'] = cmp.mapping(function(fallback)
-      if luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, {'i', 's'}),
-
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      local col = vim.fn.col('.') - 1
-
-      if cmp.visible() then
-        cmp.select_next_item(select_opts)
-      elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-        fallback()
-      else
-        cmp.complete()
-      end
-    end, {'i', 's'}),
-
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item(select_opts)
-      else
-        fallback()
-      end
-    end, {'i', 's'}),
-  },
-})
-
+ 
+ -- Set leader key
+ g.mapleader = " "
+ 
+ -- Undo files
+ opt.undofile = true
+ opt.undodir = "/home/josh/.local/share/nvim/undo"
+ 
+ -- Indentation
+ opt.smartindent = true
+ opt.autoindent = true
+ opt.tabstop = 2
+ opt.shiftwidth = 2
+ opt.softtabstop = 2
+ opt.expandtab = true
+ 
+ opt.scrolloff = 8
+ opt.updatetime = 250
+ opt.showmatch = true
+ -- Clipboard mode
+ opt.clipboard = "unnamedplus"
+ 
+ -- Allow mouse
+ opt.mouse = "a"
+ 
+ -- UI
+ opt.termguicolors = true -- causing weird colors
+ opt.cursorline = true
+ 
+ -- Hybrid line numbers
+ opt.number = true
+ opt.relativenumber = true
+ 
+ -- Line context
+ opt.list = true
+ require("indent_blankline").setup{
+   space_char_blankline = " ",
+   show_current_context = true,
+   show_current_context_start = true,
+ }
+ 
+ -- Remove viminfo
+ opt.viminfo = ""
+ opt.viminfofile = "NONE"
+ 
+ -- QoL
+ opt.smartcase = true
+ opt.smartcase = true
+ -- opt.ttimeoutlen = 5
+ opt.incsearch = true
+ opt.hlsearch = true
+ opt.autoread = true
+ require'colorizer'.setup()
+ require('lualine').setup{
+   options = {
+     theme = 'auto'
+   }
+ }
+ 
+ 
+ -- Latex
+ g.vimtex_view_method = "zathura"
+ g.vimtex_quickfix_mode = 0
+ g.vimtex_flavor = "latex"
+ g.vimtex_conceal = "abdmgs"
+ vim.cmd [[ 
+     set conceallevel=2
+     hi clear Conceal
+ ]]
+ 
+ 
+ opt.completeopt = {'menu', 'menuone', 'noselect'}
+ 
+ 
+ local cmp = require('cmp')
+ local luasnip = require('luasnip')
+ 
+ local select_opts = {behavior = cmp.SelectBehavior.Select}
+ 
 g.loaded_perl_provider = 0
-vim.cmd('colorscheme habamax')
+vim.cmd('colorscheme monokai')
 
 -- Highlight yank'd text after yankin'
 vim.api.nvim_create_augroup("YankHighlight", {})
@@ -191,3 +91,44 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 100 })
 	end,
 })
+
+
+require'nvim-treesitter.configs'.setup {
+  highlight = {
+    enable = true,
+  },
+  indent = {
+    enable = true
+  },
+  context_commentstring = {
+    enable = true
+  }
+}
+
+
+-- Autopairs
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+
+require'nvim-autopairs'.setup {
+  check_ts = true,
+  ts_config = {
+    lua = {'string', 'source'},
+
+  },
+  disable_filetype = { "TelescopePrompt" , "vim" },
+  fast_wrap = {
+    map = '<M-e>',
+    chars = { '{', '[', '(', '"', "'" },
+    pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], '%s+', ''),
+    end_key = '¤',
+    keys = 'qwertyuiopzxcvbnmasdfghjkl',
+    check_comma = true,
+    highlight = 'Search',
+    highlight_grey = "LineNr"
+  }
+}
+
+cmp.event:on(
+  'confirm_done',
+  cmp_autopairs.on_confirm_done()
+)
--- modules/home-manager/nvim/nvim.nix
+++ modules/home-manager/nvim/nvim.nix
@@ -1,13 +1,25 @@
-{config, pkgs, ...}:
+{config, pkgs, lib, ...}:
+let
+  monokai-nvim = pkgs.vimUtils.buildVimPlugin {
+    name="monokai-nvim";
+    src = pkgs.fetchFromGitHub {
+      owner = "tanvirtin";
+      repo = "monokai.nvim";
+      rev = "master";
+      sha256 = "sha256-++gmw/LWdb6+WV/t/w5KZ6D59Nwxi0DXFbDNE9fUvAA=";
+    };
+  };
+in
+  {
 
-{
   programs.neovim = {
     enable = true;
     package = pkgs.neovim-unwrapped;
     extraPackages = with pkgs; [
-      nodejs-16_x
+      nodejs-16_x # Required for copilot
       rnix-lsp
       sumneko-lua-language-server
+      haskell-language-server
       xdotool
     ];
     extraPython3Packages = ( ps: with ps; [
@@ -16,12 +28,13 @@
     viAlias = true;
     vimAlias = true;
     withNodeJs = false;
-    withPython3 = true;
+    withPython3 = true; # Used for sympy
     vimdiffAlias = true;
     extraConfig = ''
       luafile ${./init.lua}
-      luafile ${./lsp.lua}
       luafile ${./snippets.lua}
+      luafile ${./lsp.lua}
+
       highlight LineNrAbove guifg=#401580
       highlight LineNrBelow guifg=#401580
 
@@ -29,16 +42,23 @@
       \ [ 'dotnet',
       \   'fsautocomplete'
       \ ]
-      inoremap { {}<Esc>ha
-      inoremap ( ()<Esc>ha
-      inoremap [ []<Esc>ha
-      inoremap " ""<Esc>ha
     '';
     plugins = with pkgs.vimPlugins; [
+      # Theme
+      monokai-nvim
+      # UI
+      lualine-nvim
+      nvim-colorizer-lua
+
+      telescope-nvim
+      telescope-file-browser-nvim
+      indent-blankline-nvim
+      harpoon
       # LSP
       nvim-lspconfig
       nvim-cmp
       cmp-nvim-lsp
+      cmp-nvim-lsp-signature-help
       julia-vim
       Ionide-vim
       haskell-vim
@@ -53,26 +73,32 @@
       cmp-latex-symbols
       friendly-snippets
       vim-surround
+      nvim-autopairs
+
       # Treesitter
-      (nvim-treesitter.withPlugins (plugins:
-        with plugins; [
-          tree-sitter-python
-          tree-sitter-julia
-          tree-sitter-nix
-          tree-sitter-c
-          tree-sitter-rust
-          tree-sitter-toml
-          tree-sitter-json
-          tree-sitter-regex
-          tree-sitter-html
-          tree-sitter-css
-          tree-sitter-latex
-          tree-sitter-bibtex
-          tree-sitter-lua
-          tree-sitter-haskell
-          tree-sitter-dockerfile
-          tree-sitter-r
-        ]))
+      (nvim-treesitter.withPlugins (p: with p; [
+        nvim-ts-context-commentstring
+        #playground
+        julia
+        haskell
+        nix
+        latex
+        python
+        lua
+        rust
+        c
+        cpp
+        bash
+        vim
+        toml
+        json
+        yaml
+        html
+        css
+      ]))
+      nvim-treesitter-context
+      nvim-treesitter-textobjects
+      #nvim-treesitter-refactor
     ];
   };
 }

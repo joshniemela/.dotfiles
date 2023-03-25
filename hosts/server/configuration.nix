ca0c4e9ec74354b615834f30053a2469e90f5b2c
Joshua Niemela <josh@jniemela.dk>
2023-03-26 00:46:58 +0100

    Added ssh for user

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -74,7 +74,7 @@
     users.jakupl = {
       isNormalUser = true;
       initialPassword = "1234";
-      openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFkFDwJpcAKRArAOvx/fT2J5clly89NYFIdcWUVsxGRw josh@desktop"];
+      openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINEZi2SZ9MRpD8sN71gj9SxixsCMlLKADrj9r77x/H7k jakupl@pingu" ];
       shell = pkgs.zsh;
     };
 

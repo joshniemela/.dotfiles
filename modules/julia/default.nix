9de1fb245ebe156bab90bde9961c31d8630e9941
Joshua Niemela <josh@jniemela.dk>
2023-06-11 15:04:57 +0200

    removed julia19 since it is now the standard version

--- modules/julia/default.nix
+++ modules/julia/default.nix
@@ -20,22 +20,6 @@
             ${pkgs.julia_19}/bin/julia "$@" ''
       ) {}
     )
-    (
-      pkgs.callPackage (
-        {
-          pkgs,
-          writeShellScriptBin,
-          stdenv,
-          config,
-          ...
-        }:
-          pkgs.writeShellScriptBin "julia19" ''
-            export JULIA_NUM_THREADS="auto";
-            export NIX_LD=${stdenv.cc.libc}/lib/ld-linux-x86-64.so.2
-            export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/run/opengl-driver/lib"
-            ~/.juliaup/bin/julia +beta "$@" ''
-      ) {}
-    )
   ];
 
   home.file.".julia/config/startup.jl".source = ./startup.jl;

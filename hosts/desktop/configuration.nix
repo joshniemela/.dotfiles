c1de923d688f8783485181123b3677d7b8a4d891
Joshua Niemela <josh@jniemela.dk>
2024-05-26 15:21:54 +0200

    Merged

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -12,7 +12,7 @@
     ../../modules/doas.nix
     ../default/configuration.nix # default host config
     ../../modules/docker.nix
-    ../../modules/virtualbox.nix
+    #../../modules/virtualbox.nix
   ];
 
   boot = {

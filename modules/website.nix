b3087db6934ee00778990a40cfc6e48317f08d6b
Joshua Niemelä <josh@jniemela.dk>
2024-08-08 13:34:54 +0200

    Added nginx settings

--- modules/website.nix
+++ modules/website.nix
@@ -7,6 +7,10 @@
   security.acme.acceptTerms = true;
   security.acme.defaults.email = "josh@jniemela.dk";
   services.nginx = {
+    recommendedProxySettings = true;
+    recommendedOptimisation = true;
+    recommendedGzipSettings = true;
+    recommendedTlsSettings = true;
     enable = true;
     virtualHosts."jniemela.dk" = {
       enableACME = true;
@@ -19,6 +23,25 @@
             try_files $uri $uri.html $uri/ =404;
       '';
       root = "/var/www";
+
+
+      locations."/predict" = {
+        proxyPass = "http://localhost:4242";
+        extraConfig =
+          "proxy_ssl_server_name on;"
+          + "proxy_pass_header Authorization;";
+      };
+    };
+    virtualHosts."filebrowser.jniemela.dk" = {
+      enableACME = true;
+      forceSSL = true;
+
+      locations."/" = {
+        proxyPass = "http://localhost:8080";
+        extraConfig =
+          "proxy_ssl_server_name on;"
+          + "proxy_pass_header Authorization;";
+      };
     };
     virtualHosts."disku.jniemela.dk" = {
       enableACME = true;

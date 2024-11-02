e810b7566071f2e7fdebf97014be8dbb115eab54
Joshua Niemelä <josh@jniemela.dk>
2024-11-02 14:21:20 +0100

    .

--- home-manager/nvim/nvim.nix
+++ home-manager/nvim/nvim.nix
@@ -20,55 +20,55 @@ in {
     extraPackages = with pkgs; [
       xdotool
       sumneko-lua-language-server
-      ripgrep
-      pyright
-      black
-      # haskell-language-server disabled due to haskell-tools-nvim containing HLS itself
+      #ripgrep
+      #pyright
+      #black
+      #haskell-language-server disabled due to haskell-tools-nvim containing HLS itself
     ];
     viAlias = true;
     vimAlias = true;
-    withNodeJs = true;
+    withNodeJs = false;
     withPython3 = false;
     vimdiffAlias = true;
     extraConfig = ''
       luafile ${./init.lua}
-      luafile ${./telescope.lua}
       luafile ${./treesitter.lua}
-      luafile ${./harpoon.lua}
-      luafile ${./lsp.lua}
     '';
+    #luafile ${./telescope.lua}
+    #luafile ${./harpoon.lua}
+    #luafile ${./lsp.lua}
 
     #luafile ${./snippets.lua}
     #luafile ${./cmp.lua}
     plugins = with pkgs.vimPlugins; [
       indent-blankline-nvim
-      lualine-nvim
-      nvim-colorizer-lua
+      #lualine-nvim
+      #nvim-colorizer-lua
       # Theme
       monokai-nvim
       # UI
-      vimtex
-      luasnip
-      nvim-cmp
-      cmp_luasnip
-      cmp-path
-      cmp-buffer
-      cmp-latex-symbols
-      cmp-nvim-lsp
-      cmp-nvim-lsp-signature-help
-      copilot-vim
-      null-ls-nvim
-      nvim-lspconfig
+      #vimtex
+      #luasnip
+      #nvim-cmp
+      #cmp_luasnip
+      #cmp-path
+      #cmp-buffer
+      #cmp-latex-symbols
+      #cmp-nvim-lsp
+      #cmp-nvim-lsp-signature-help
+      #copilot-vim
+      #null-ls-nvim
+      #nvim-lspconfig
 
-      which-key-nvim
-      plenary-nvim
-      haskell-tools-nvim
-      vim-nix
-      julia-vim
+      #which-key-nvim
+      #plenary-nvim
+      #haskell-tools-nvim
+      #vim-nix
+      #julia-vim
 
-      # new nvim
-      telescope-nvim
-      harpoon
+      ## new nvim
+      #telescope-nvim
+      #harpoon
 
       (nvim-treesitter.withPlugins (p:
         with p; [
--- home-manager/nvim/treesitter.lua
+++ home-manager/nvim/treesitter.lua
@@ -1,3 +1,4 @@
+require'ibl'.setup {}
 require'nvim-treesitter.configs'.setup {
   sync_install = false,
   auto_install = false,

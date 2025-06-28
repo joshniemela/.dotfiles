fc0bea772ac245ac7b05451e1fc6320c7adea807
Joshua Niemelä <josh@jniemela.dk>
2025-06-28 14:45:06 +0200

    Split xmobar into laptop and desktop

--- home-manager/xmonad/default.nix
+++ home-manager/xmonad/default.nix
@@ -2,32 +2,36 @@
   config,
   pkgs,
   lib,
+  osConfig,
   ...
 }: {
-  config = {
-    home.packages = with pkgs; [
-      haskellPackages.haskell-language-server
-      haskellPackages.hoogle
-      haskellPackages.xmobar
-      cabal-install
-      ghc
-      zenity
-    ];
+  config = lib.mkMerge [
+    {
+      home.packages = with pkgs; [
+        haskellPackages.haskell-language-server
+        haskellPackages.hoogle
+        haskellPackages.xmobar
+        cabal-install
+        ghc
+        zenity
+      ];
 
-    home.file.".xmonad/xmobar.hs".source = ./xmobar.hs;
-    # programs.xmobar = {
-    #   enable = true;
-    #   extraConfig = lib.readFile ./xmobarrc;
-    # };
-
-    xsession = {
-      enable = true;
-
-      windowManager.xmonad = {
+      xsession = {
         enable = true;
-        enableContribAndExtras = true;
-        config = ./xmonad.hs;
+
+        windowManager.xmonad = {
+          enable = true;
+          enableContribAndExtras = true;
+          config = ./xmonad.hs;
+        };
       };
-    };
-  };
+    }
+    (lib.mkIf (osConfig.networking.hostName == "desktop") {
+      home.file.".xmonad/xmobar.hs".source = ./xmobar-desktop.hs;
+    })
+
+    (lib.mkIf (osConfig.networking.hostName == "thonkpad") {
+      home.file.".xmonad/xmobar.hs".source = ./xmobar-thonkpad.hs;
+    })
+  ];
 }

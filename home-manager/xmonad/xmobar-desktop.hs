255ecc4e772f2e4b9750ccb65ca80651a8ae1714
Joshua Niemelä <josh@jniemela.dk>
2025-07-01 16:54:07 +0200

    Format

--- home-manager/xmonad/xmobar-desktop.hs
+++ home-manager/xmonad/xmobar-desktop.hs
@@ -1,117 +1,116 @@
 Config
-  { -- appearance
-    font = "DejaVu Sans Mono Bold 10",
-    bgColor = "black",
-    fgColor = "#646464",
-    position = BottomH 16,
-    border = TopB,
-    borderColor = "#646464",
-    -- layout
-    sepChar = "%", -- delineator between plugin names and straight text
-    alignSep = "}{", -- separator between left-right alignment
-    template = "%StdinReader% | %multicpu% | %coretemp% | %memory% | %dynnetwork% }{ %EKCH% | %date%",
-    -- general behavior
-    lowerOnStart = True, -- send to bottom of window stack on start
-    hideOnStart = False, -- start with window unmapped (hidden)
-    allDesktops = True, -- show on all desktops
-    overrideRedirect = True, -- set the Override Redirect flag (Xlib)
-    pickBroadest = True, -- choose widest display (multi-monitor)
-    persistent = True, -- enable/disable hiding (True = disabled)
-
-    -- plugins
-    --   Numbers can be automatically colored according to their value. xmobar
-    --   decides color based on a three-tier/two-cutoff system, controlled by
-    --   command options:
-    --     --Low sets the low cutoff
-    --     --High sets the high cutoff
-    --
-    --     --low sets the color below --Low cutoff
-    --     --normal sets the color between --Low and --High cutoffs
-    --     --High sets the color above --High cutoff
-    --
-    --   The --template option controls how the plugin is displayed. Text
-    --   color can be set by enclosing in <fc></fc> tags. For more details
-    --   see http://projects.haskell.org/xmobar/#system-monitor-plugins.
-    commands =
-      -- weather monitor
-      [ Run
-          Weather
-          "EKCH"
-          [ "--template",
-            "<skyCondition> | <fc=#4682B4><tempC></fc>°C | <fc=#4682B4><rh></fc>% | <fc=#4682B4><pressure></fc>hPa"
-          ]
-          3600,
-        -- network activity monitor (dynamic interface resolution)
-        Run
-          DynNetwork
-          [ "--template",
-            "<dev>: <tx>kB/s|<rx>kB/s",
-            "--Low",
-            "1000", -- units: B/s
-            "--High",
-            "5000", -- units: B/s
-            "--low",
-            "darkgreen",
-            "--normal",
-            "darkorange",
-            "--high",
-            "darkred"
-          ]
-          10,
-        -- cpu activity monitor
-        Run
-          MultiCpu
-          [ "--template",
-            "CPU: <total0>%|<total1>%",
-            "--Low",
-            "50", -- units: %
-            "--High",
-            "85", -- units: %
-            "--low",
-            "darkgreen",
-            "--normal",
-            "darkorange",
-            "--high",
-            "darkred"
-          ]
-          10,
-        -- cpu core temperature monitor
-        Run
-          CoreTemp
-          [ "--template",
-            "Temp: <core0>°C|<core1>°C",
-            "--Low",
-            "70", -- units: °C
-            "--High",
-            "80", -- units: °C
-            "--low",
-            "darkgreen",
-            "--normal",
-            "darkorange",
-            "--high",
-            "darkred"
-          ]
-          50,
-        -- memory usage monitor
-        Run
-          Memory
-          [ "--template",
-            "Mem: <usedratio>%",
-            "--Low",
-            "20", -- units: %
-            "--High",
-            "90", -- units: %
-            "--low",
-            "darkgreen",
-            "--normal",
-            "darkorange",
-            "--high",
-            "darkred"
-          ]
-          10,
-        -- time and date indicator
-        --   (%F = y-m-d date, %a = day of week, %T = h:m:s time)
-        Run Date "<fc=#656565> Week %V %F (%a) %T</fc>" "date" 10,
-        Run StdinReader
-      ]
-  }
+    { -- appearance
+      font = "DejaVu Sans Mono Bold 10"
+    , bgColor = "black"
+    , fgColor = "#646464"
+    , position = BottomH 16
+    , border = TopB
+    , borderColor = "#646464"
+    , -- layout
+      sepChar = "%" -- delineator between plugin names and straight text
+    , alignSep = "}{" -- separator between left-right alignment
+    , template = "%StdinReader% | %multicpu% | %coretemp% | %memory% | %dynnetwork% }{ %EKCH% | %date%"
+    , -- general behavior
+      lowerOnStart = True -- send to bottom of window stack on start
+    , hideOnStart = False -- start with window unmapped (hidden)
+    , allDesktops = True -- show on all desktops
+    , overrideRedirect = True -- set the Override Redirect flag (Xlib)
+    , pickBroadest = True -- choose widest display (multi-monitor)
+    , persistent = True -- enable/disable hiding (True = disabled)
+    , -- plugins
+      --   Numbers can be automatically colored according to their value. xmobar
+      --   decides color based on a three-tier/two-cutoff system, controlled by
+      --   command options:
+      --     --Low sets the low cutoff
+      --     --High sets the high cutoff
+      --
+      --     --low sets the color below --Low cutoff
+      --     --normal sets the color between --Low and --High cutoffs
+      --     --High sets the color above --High cutoff
+      --
+      --   The --template option controls how the plugin is displayed. Text
+      --   color can be set by enclosing in <fc></fc> tags. For more details
+      --   see http://projects.haskell.org/xmobar/#system-monitor-plugins.
+      commands =
+        -- weather monitor
+        [ Run
+            Weather
+            "EKCH"
+            [ "--template"
+            , "<skyCondition> | <fc=#4682B4><tempC></fc>°C | <fc=#4682B4><rh></fc>% | <fc=#4682B4><pressure></fc>hPa"
+            ]
+            3600
+        , -- network activity monitor (dynamic interface resolution)
+          Run
+            DynNetwork
+            [ "--template"
+            , "<dev>: <tx>kB/s|<rx>kB/s"
+            , "--Low"
+            , "1000" -- units: B/s
+            , "--High"
+            , "5000" -- units: B/s
+            , "--low"
+            , "darkgreen"
+            , "--normal"
+            , "darkorange"
+            , "--high"
+            , "darkred"
+            ]
+            10
+        , -- cpu activity monitor
+          Run
+            MultiCpu
+            [ "--template"
+            , "CPU: <total0>%|<total1>%"
+            , "--Low"
+            , "50" -- units: %
+            , "--High"
+            , "85" -- units: %
+            , "--low"
+            , "darkgreen"
+            , "--normal"
+            , "darkorange"
+            , "--high"
+            , "darkred"
+            ]
+            10
+        , -- cpu core temperature monitor
+          Run
+            CoreTemp
+            [ "--template"
+            , "Temp: <core0>°C|<core1>°C"
+            , "--Low"
+            , "70" -- units: °C
+            , "--High"
+            , "80" -- units: °C
+            , "--low"
+            , "darkgreen"
+            , "--normal"
+            , "darkorange"
+            , "--high"
+            , "darkred"
+            ]
+            50
+        , -- memory usage monitor
+          Run
+            Memory
+            [ "--template"
+            , "Mem: <usedratio>%"
+            , "--Low"
+            , "20" -- units: %
+            , "--High"
+            , "90" -- units: %
+            , "--low"
+            , "darkgreen"
+            , "--normal"
+            , "darkorange"
+            , "--high"
+            , "darkred"
+            ]
+            10
+        , -- time and date indicator
+          --   (%F = y-m-d date, %a = day of week, %T = h:m:s time)
+          Run Date "<fc=#656565> Week %V %F (%a) %T</fc>" "date" 10
+        , Run StdinReader
+        ]
+    }
--- home-manager/xmonad/xmonad.hs
+++ home-manager/xmonad/xmonad.hs
@@ -1,3 +1,5 @@
+import Control.Monad (forM_)
+import Data.List (isPrefixOf)
 import qualified Data.Map as M
 import System.Directory
 import System.IO
@@ -18,8 +20,6 @@ import XMonad.Util.NamedActions
 import XMonad.Util.NamedScratchpad
 import XMonad.Util.Run
 import XMonad.Util.SpawnOnce
-import Data.List (isPrefixOf)
-import Control.Monad (forM_)
 
 myTerminal = "kitty"
 
@@ -42,153 +42,155 @@ myModMask = mod1Mask -- 3 is right alt and 4 is super
 
 myKeys :: XConfig Layout -> [((KeyMask, KeySym), NamedAction)]
 myKeys c =
-  let subKeys str ks = subtitle str : ks
-   in subKeys
-        "Essentials"
-        [ ((myModMask, xK_q), addName "Recompile/restart XMonad" $ spawn "xmonad --recompile && pkill xmobar && xmonad --restart"),
-          ((myModMask .|. shiftMask, xK_Return), addName "Open terminal" $ spawn myTerminal),
-          ((myModMask .|. shiftMask, xK_c), addName "Close window" kill),
-          ((myModMask, xK_p), addName "Open dmenu" $ spawn "dmenu_run -sb '#402F65'"),
-          ((myModMask .|. shiftMask, xK_p), addName "Open passmenu" $ spawn "passmenu -sb '#D16328'")
-        ]
-        ++ subtitle "Switching workspaces"
-        :
-        -- mod-[1..9] %! Switch to workspace N
-        -- mod-shift-[1..9] %! Move client to workspace N
-        [ ((m .|. myModMask, k), addName (n ++ show i) $ handleWorkspaceKey i f)
-          | (f, m, n) <- [
-              (W.greedyView, 0, "Switch to workspace "), 
-              (W.shift, shiftMask, "Move client to workspace ")],
-            (i, k) <- zip [1..9] [xK_1 .. xK_9]
-        ]
-        ++
-        [ ((myModMask .|. shiftMask, xK_z), addName "Swap workspace sets" $ do
-            currentSet <- XS.get
-            let newSet = switch currentSet
-            XS.put newSet
-        
-            screens <- gets (W.screens . windowset)
-        
-            let targetWs = case newSet of
-                  Personal -> myPersonalWorkspaces
-                  Work     -> myWorkWorkspaces
-        
-                workspaces = take (length screens) targetWs
-        
-            -- For each screen, focus it and greedyView its new workspace
-            forM_ (zip screens workspaces) $ \(s, ws) -> do
-              windows (W.view (W.tag (W.workspace s))) -- focus the screen
-              windows (W.greedyView ws)
-          )]
-        ++ subtitle "Switching screens"
-        :
-        -- mod-{w,e,r} %! Switch to physical/Xinerama screens 1, 2, or 3
-        -- mod-shift-{w,e,r} %! Move client to screen 1, 2, or 3
-        [ ((m .|. myModMask, key), addName (n ++ show sc) $ screenWorkspace sc >>= flip whenJust (windows . f))
-          | (f, m, n) <- [(W.view, 0, "Switch to screen number "), (W.shift, shiftMask, "Move client to screen number ")],
-            (key, sc) <- zip [xK_w, xK_e, xK_r] [0 ..]
-        ]
-        ++ subKeys
-          "Changing layouts"
-          [ ((myModMask, xK_space), sendMessage' NextLayout),
-            ((myModMask .|. shiftMask, xK_space), addName "Reset the layout" $ setLayout $ XMonad.layoutHook c)
-          ]
-        ++ subKeys
-          "Resizing"
-          [ ((myModMask, xK_h), sendMessage' Shrink),
-            ((myModMask, xK_l), sendMessage' Expand)
-          ]
-        ++ subKeys
-          "Focus"
-          [ ((myModMask, xK_Tab), addName "Focus down" $ windows W.focusDown),
-            ((myModMask .|. shiftMask, xK_Tab), addName "Focus up" $ windows W.focusUp),
-            ((myModMask, xK_j), addName "Focus down" $ windows W.focusDown),
-            ((myModMask, xK_k), addName "Focus up" $ windows W.focusUp),
-            ((myModMask, xK_m), addName "Focus the master" $ windows W.focusMaster)
-          ]
-        ++ subKeys
-          "Modifying window order"
-          [ ((myModMask, xK_Return), addName "Swap with the master" $ windows W.swapMaster),
-            ((myModMask .|. shiftMask, xK_j), addName "Swap down" $ windows W.swapDown),
-            ((myModMask .|. shiftMask, xK_k), addName "Swap up" $ windows W.swapUp)
-          ]
-        ++ subKeys
-          "Floating windows"
-          [ ((myModMask, xK_t), addName "Push focused floating to tiled" $ withFocused $ windows . W.sink),
-            ((myModMask .|. shiftMask, xK_t), addName "Push all floating to tiled" sinkAll),
-            ((myModMask, xK_f), addName "Push focused to floating" $ sendMessage $ T.Toggle "floats")
-          ]
-        ++ subKeys
-          "Other stuff"
-          [ ((noModMask, xK_Print), addName "Take screenshot" $ spawn "flameshot gui")
-          ]
-        ++ subKeys
-          "Scratchpads"
-          [ ((myModMask, xK_Return), addName "Open terminal" $ namedScratchpadAction myScratchPads "term")
-          ]
+    let subKeys str ks = subtitle str : ks
+     in subKeys
+            "Essentials"
+            [ ((myModMask, xK_q), addName "Recompile/restart XMonad" $ spawn "xmonad --recompile && pkill xmobar && xmonad --restart")
+            , ((myModMask .|. shiftMask, xK_Return), addName "Open terminal" $ spawn myTerminal)
+            , ((myModMask .|. shiftMask, xK_c), addName "Close window" kill)
+            , ((myModMask, xK_p), addName "Open dmenu" $ spawn "dmenu_run -sb '#402F65'")
+            , ((myModMask .|. shiftMask, xK_p), addName "Open passmenu" $ spawn "passmenu -sb '#D16328'")
+            ]
+            ++ subtitle "Switching workspaces"
+            :
+            -- mod-[1..9] %! Switch to workspace N
+            -- mod-shift-[1..9] %! Move client to workspace N
+            [ ((m .|. myModMask, k), addName (n ++ show i) $ handleWorkspaceKey i f)
+            | (f, m, n) <-
+                [ (W.greedyView, 0, "Switch to workspace ")
+                , (W.shift, shiftMask, "Move client to workspace ")
+                ]
+            , (i, k) <- zip [1 .. 9] [xK_1 .. xK_9]
+            ]
+            ++ [
+                   ( (myModMask .|. shiftMask, xK_z)
+                   , addName "Swap workspace sets" $ do
+                        currentSet <- XS.get
+                        let newSet = switch currentSet
+                        XS.put newSet
+
+                        screens <- gets (W.screens . windowset)
+
+                        let targetWs = case newSet of
+                                Personal -> myPersonalWorkspaces
+                                Work -> myWorkWorkspaces
+
+                            workspaces = take (length screens) targetWs
+
+                        -- For each screen, focus it and greedyView its new workspace
+                        forM_ (zip screens workspaces) $ \(s, ws) -> do
+                            windows (W.view (W.tag (W.workspace s))) -- focus the screen
+                            windows (W.greedyView ws)
+                   )
+               ]
+            ++ subtitle "Switching screens"
+            :
+            -- mod-{w,e,r} %! Switch to physical/Xinerama screens 1, 2, or 3
+            -- mod-shift-{w,e,r} %! Move client to screen 1, 2, or 3
+            [ ((m .|. myModMask, key), addName (n ++ show sc) $ screenWorkspace sc >>= flip whenJust (windows . f))
+            | (f, m, n) <- [(W.view, 0, "Switch to screen number "), (W.shift, shiftMask, "Move client to screen number ")]
+            , (key, sc) <- zip [xK_w, xK_e, xK_r] [0 ..]
+            ]
+            ++ subKeys
+                "Changing layouts"
+                [ ((myModMask, xK_space), sendMessage' NextLayout)
+                , ((myModMask .|. shiftMask, xK_space), addName "Reset the layout" $ setLayout $ XMonad.layoutHook c)
+                ]
+            ++ subKeys
+                "Resizing"
+                [ ((myModMask, xK_h), sendMessage' Shrink)
+                , ((myModMask, xK_l), sendMessage' Expand)
+                ]
+            ++ subKeys
+                "Focus"
+                [ ((myModMask, xK_Tab), addName "Focus down" $ windows W.focusDown)
+                , ((myModMask .|. shiftMask, xK_Tab), addName "Focus up" $ windows W.focusUp)
+                , ((myModMask, xK_j), addName "Focus down" $ windows W.focusDown)
+                , ((myModMask, xK_k), addName "Focus up" $ windows W.focusUp)
+                , ((myModMask, xK_m), addName "Focus the master" $ windows W.focusMaster)
+                ]
+            ++ subKeys
+                "Modifying window order"
+                [ ((myModMask, xK_Return), addName "Swap with the master" $ windows W.swapMaster)
+                , ((myModMask .|. shiftMask, xK_j), addName "Swap down" $ windows W.swapDown)
+                , ((myModMask .|. shiftMask, xK_k), addName "Swap up" $ windows W.swapUp)
+                ]
+            ++ subKeys
+                "Floating windows"
+                [ ((myModMask, xK_t), addName "Push focused floating to tiled" $ withFocused $ windows . W.sink)
+                , ((myModMask .|. shiftMask, xK_t), addName "Push all floating to tiled" sinkAll)
+                , ((myModMask, xK_f), addName "Push focused to floating" $ sendMessage $ T.Toggle "floats")
+                ]
+            ++ subKeys
+                "Other stuff"
+                [ ((noModMask, xK_Print), addName "Take screenshot" $ spawn "flameshot gui")
+                ]
+            ++ subKeys
+                "Scratchpads"
+                [ ((myModMask, xK_Return), addName "Open terminal" $ namedScratchpadAction myScratchPads "term")
+                ]
 
 showKeybindings :: [((KeyMask, KeySym), NamedAction)] -> NamedAction
 showKeybindings x = addName "Show Keybindings" $ io $ do
-  h <- spawnPipe "zenity --text-info --font=terminus"
-  hPutStr h (unlines $ showKm x)
-  hClose h
-  return ()
+    h <- spawnPipe "zenity --text-info --font=terminus"
+    hPutStr h (unlines $ showKm x)
+    hClose h
+    return ()
 
 myLogHook h = do
-  state <- XS.get
-  let filterWorkspaceByState = filterByPrefix (prefix state)
+    state <- XS.get
+    let filterWorkspaceByState = filterByPrefix (prefix state)
 
-  dynamicLogWithPP $
-    def
-      { ppLayout = wrap "(<fc=#e4b63c>" "</fc>)",
-        ppHidden = filterWorkspaceByState id,
-        -- , ppSort = getSortByXineramaRule  -- Sort left/right screens on the left, non-empty workspaces after those
-        ppTitleSanitize = const "", -- Also about window's title
-        ppVisible = filterWorkspaceByState (wrap "(" ")"), -- Non-focused (but still visible) screen
-        ppCurrent = wrap "<fc=#b8473d>[</fc><fc=#7cac7a>" "</fc><fc=#b8473d>]</fc>", -- Focused screen
-        ppOutput = hPutStrLn h
-      }
+    dynamicLogWithPP $
+        def
+            { ppLayout = wrap "(<fc=#e4b63c>" "</fc>)"
+            , ppHidden = filterWorkspaceByState id
+            , -- , ppSort = getSortByXineramaRule  -- Sort left/right screens on the left, non-empty workspaces after those
+              ppTitleSanitize = const "" -- Also about window's title
+            , ppVisible = filterWorkspaceByState (wrap "(" ")") -- Non-focused (but still visible) screen
+            , ppCurrent = wrap "<fc=#b8473d>[</fc><fc=#7cac7a>" "</fc><fc=#b8473d>]</fc>" -- Focused screen
+            , ppOutput = hPutStrLn h
+            }
 
 myManageHook = (manageDocks <+> manageHook def) <+> namedScratchpadManageHook myScratchPads
 
 myLayoutHook = avoidStruts $ layoutHook def
 
 myStartupHook = do
-  spawnOnce "autorandr -c"
+    spawnOnce "autorandr -c"
 
 myConfig statusPipe =
-  def
-    { -- simple stuff
-      terminal = myTerminal,
-      focusFollowsMouse = myFocusFollowsMouse,
-      modMask = myModMask,
-      workspaces = myWorkspaces,
-      focusedBorderColor = myFocusedBorderColor,
-      normalBorderColor = myNormalBorderColor,
-      -- hooks, layouts
-      logHook = myLogHook statusPipe,
-      manageHook = myManageHook,
-      layoutHook = myLayoutHook,
-      startupHook = myStartupHook
-    }
+    def
+        { -- simple stuff
+          terminal = myTerminal
+        , focusFollowsMouse = myFocusFollowsMouse
+        , modMask = myModMask
+        , workspaces = myWorkspaces
+        , focusedBorderColor = myFocusedBorderColor
+        , normalBorderColor = myNormalBorderColor
+        , -- hooks, layouts
+          logHook = myLogHook statusPipe
+        , manageHook = myManageHook
+        , layoutHook = myLayoutHook
+        , startupHook = myStartupHook
+        }
 
 main = do
-  statusPipe <- spawnPipe "xmobar -x 0 ~/.xmonad/xmobar.hs"
-  xmonad $
-    docks $
-      ewmhFullscreen $
-        ewmh $
-          addDescrKeys' ((myModMask, xK_F1), showKeybindings) myKeys $
-            myConfig statusPipe
+    statusPipe <- spawnPipe "xmobar -x 0 ~/.xmonad/xmobar.hs"
+    xmonad $
+        docks $
+            ewmhFullscreen $
+                ewmh $
+                    addDescrKeys' ((myModMask, xK_F1), showKeybindings) myKeys $
+                        myConfig statusPipe
 
 myScratchPads =
-  [ NS "term" spawnTerm findTerm manageTerm
-  ]
+    [ NS "term" spawnTerm findTerm manageTerm
+    ]
   where
     spawnTerm = myTerminal ++ " --class=term"
     findTerm = className =? "term"
     manageTerm = defaultFloating
-
 
 data WorkspacesType = Personal | Work deriving (Show, Read, Eq)
 
@@ -202,26 +204,19 @@ prefix Work = "w"
 
 filterByPrefix :: String -> (String -> String) -> String -> String
 filterByPrefix prefix wrapFunc ws =
-  if prefix `isPrefixOf` ws
-    then wrapFunc ws
-    else ""
+    if prefix `isPrefixOf` ws
+        then wrapFunc ws
+        else ""
 
 instance ExtensionClass WorkspacesType where
-  initialValue = Personal
-  extensionType = PersistentExtension
-
-
-
-
+    initialValue = Personal
+    extensionType = PersistentExtension
 
 handleWorkspaceKey :: Int -> (WorkspaceId -> WindowSet -> WindowSet) -> X ()
 handleWorkspaceKey i action = do
-  currentSet <- XS.get
-  let workspaces = case currentSet of
-        Personal -> myPersonalWorkspaces
-        Work     -> myWorkWorkspaces
-  let targetWs = workspaces !! (i - 1)
-  windows $ action targetWs
-
-
-
+    currentSet <- XS.get
+    let workspaces = case currentSet of
+            Personal -> myPersonalWorkspaces
+            Work -> myWorkWorkspaces
+    let targetWs = workspaces !! (i - 1)
+    windows $ action targetWs

708003c7a389aa246734f0b7f36d57b547ae79cb
Joshua Niemelä <josh@jniemela.dk>
2025-07-06 13:44:48 +0200

    Add theme and visual options

--- home-manager/nvim/default.nix
+++ home-manager/nvim/default.nix
@@ -1,8 +1,85 @@
-{inputs, ...}: {
+{ lib, inputs, ... }:
+{
   imports = [
     inputs.nvf.homeManagerModules.default
+
+    ./themes/moonfly.nix
+    ./treesitter.nix
   ];
   programs.nvf = {
     enable = true;
+    settings.vim = {
+      preventJunkFiles = true;
+      enableLuaLoader = true;
+      statusline.lualine.enable = true;
+      ui.colorizer.enable = true;
+
+      keymaps = [
+      ];
+
+      globals = {
+        mapleader = " ";
+        maplocalleader = " ";
+      };
+
+      options = {
+        autoindent = true;
+        smartindent = true;
+        tabstop = 2;
+        shiftwidth = 2;
+        softtabstop = 2;
+        expandtab = true;
+        scrolloff = 8;
+        showmatch = true;
+
+        updatetime = 100;
+        tm = 300;
+
+        # Undo file
+        undofile = true;
+
+        # UI
+        termguicolors = true;
+        cursorline = true;
+
+        # Line context
+        list = true;
+
+        # Remove viminfo
+        viminfo = "";
+        viminfofile = "NONE";
+
+        # QoL
+        ignorecase = true;
+        smartcase = true;
+        incsearch = true;
+        hlsearch = true;
+      };
+
+      clipboard = {
+        enable = true;
+        registers = "unnamedplus";
+      };
+
+      augroups = [
+        {
+          name = "highlight-yank";
+          clear = true;
+        }
+      ];
+
+      autocmds = [
+        {
+          event = [ "TextYankPost" ];
+          desc = "Highlight when yanking text";
+          group = "highlight-yank";
+          callback = lib.mkLuaInline ''
+            function()
+              vim.highlight.on_yank()
+            end
+          '';
+        }
+      ];
+    };
   };
 }
---
+++ home-manager/nvim/themes/moonfly.nix
@@ -0,0 +1,19 @@
+{inputs, pkgs, ...}: let
+  moonfly-nvim-pkg = pkgs.vimUtils.buildVimPlugin {
+    name = "moonfly-nvim";
+    src = pkgs.fetchFromGitHub {
+      owner = "bluz71";
+      repo = "vim-moonfly-colors";
+      rev = "master";
+      sha256 = "sha256-v8U6Gsm5oKZPdKxex7XcJQX3az6M47YK4H5Kfx5lzpE=";
+    };
+  };
+in {
+  programs.nvf = {
+    settings.vim.extraPlugins.moonfly = { 
+      package = moonfly-nvim-pkg;
+      setup = ''vim.cmd("colorscheme moonfly")'';
+    };
+  };
+}
+
